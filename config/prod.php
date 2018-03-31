502e93ded55dbd0c2c3e8cc470faa50512c0058d
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-03-30 22:30:08 -0400

    register doctrine db for production

--- config/prod.php
+++ config/prod.php
@@ -1,12 +1,15 @@
 <?php
 
 // configure your app for the production environment
+use Silex\Provider\DoctrineServiceProvider;
 
 $app['twig.path']    = array(__DIR__ . '/../templates');
 $app['twig.options'] = array('cache' => __DIR__ . '/../var/cache/twig');
 
 // Doctrine: DB options
-$app['db.options'] = array(
-    'driver' => 'pdo_sqlite',
-    'path'   => __DIR__ . '/../sqlite/coding-challenge.db',
-);
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__ . '/../sqlite/coding-challenge.db',
+    ),
+));
