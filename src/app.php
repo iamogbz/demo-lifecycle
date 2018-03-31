a57dfdc57b2e185635bed18ed0c103b9d91ad5b0
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-03-30 22:12:18 -0400

    login form

--- src/app.php
+++ src/app.php
@@ -13,6 +13,13 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), [
+    'translator.domains' => [],
+]);
 
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
--- src/controllers.php
+++ src/controllers.php
@@ -6,13 +6,59 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Lifecycle\User;
+
 //Request::setTrustedProxies(array('127.0.0.1'));
 
+// home controllers
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array());
+    if ($app['session']->get('user') == null) {
+        return $app->redirect('/login');
+    } else {
+        return $app->redirect('/welcome');
+    }
 })
 ->bind('homepage');
 
+// login controllers
+$app->match('/login', function (Request $request) use ($app) {
+    // $request = $app['request_stack']->getCurrentRequest();
+    $data = [
+        'error' => false,
+        'message' => '',
+        'username' => '',
+    ];
+    $form = $app['form.factory']->createBuilder(FormType::class, $data)
+        ->add('username', TextType::class, ['required' => true])
+        ->add('password', PasswordType::class, ['required' => true])
+        ->add('submit', SubmitType::class, ['label' => 'Enter'])
+        ->getForm();
+    
+    if ($request) {
+        $form->handleRequest($request);
+        $data = $form->getData();
+    }
+    if (empty($data['username']) || empty($data['password'])) {
+        // display the form
+        return $app['twig']->render('login.html.twig', $data);
+    } else {
+        $user = new User($data['username'], $app['db']);
+        $authenticated = $user->authenticate($app['db'], $data['password']);
+        if ($authenticated) {
+            $app['session']->set('user', $user);
+            return $app->redirect('/welcome');
+        } else {
+            $data['error'] = true;
+            $data['message'] = "no match found";
+            return $app['twig']->render('login.html.twig', $data);
+        }
+    }
+});
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
