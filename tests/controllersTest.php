3e8013608760686c77fcc95adcf74f73440a11d2
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-03-31 08:15:47 -0400

    test dashboard and login redirect

--- tests/controllersTest.php
+++ tests/controllersTest.php
@@ -1,8 +1,10 @@
 <?php
 
+namespace LifeCycle;
+
 use Silex\WebTestCase;
 
-class controllersTest extends WebTestCase
+class ControllersTest extends WebTestCase
 {
     public function testGetHomepage()
     {
@@ -19,8 +21,31 @@ class controllersTest extends WebTestCase
         $app = require __DIR__.'/../src/app.php';
         require __DIR__.'/../config/dev.php';
         require __DIR__.'/../src/controllers.php';
+        require_once __DIR__.'/../src/models.php';
         $app['session.test'] = true;
-
+        unset($app['exception_handler']);
         return $this->app = $app;
+    }
+
+    public function testDashboardRedirect()
+    {
+        $user = new User('janed', $this->app['db']);
+        $this->app['session']->set('user', $user);
+        $client = $this->createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('successful login', $crawler->filter('body')->text());
+    }
+
+    public function testLoginRedirect()
+    {
+        $this->app['session']->clear('user');
+        $client = $this->createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('please complete', $crawler->filter('body')->text());
     }
 }
